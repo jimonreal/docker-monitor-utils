a95e3391db1020a4d1286c3e1a2b7556497214e1
Jose I. Monreal Bailey <jmonreal@gmail.com>
2015-12-10 10:38:53 -0300

    Adding Dummy Driver

---
+++ Driver/DummyDriver.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+class DummyDriver
+
+    def initialize
+    end
+
+    def reset
+    end
+
+    def monitorContainerStats(cid, stats)
+    end
+
+    def responseExitCode
+    end
+
+    def getStatusExit
+    end
+
+end
